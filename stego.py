c3cd2fbce076431d34b2995a51d2ec2c2c2f1a0a
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-06-16 22:05:30 +0700

    update stego.py

--- stego.py
+++ stego.py
@@ -39,9 +39,9 @@ def decode():
 	audio = wave.open("sample_out.wav", mode='rb')
 	frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
 	extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-	string_out = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+	string_out = "".join(chr(int("".join(map(str, extracted[i:i+8])), 2)) for i in range(0, len(extracted), 8))
 	decoded = string_out.split("###")[0]
-	print("Sucessfully decoded: "+decoded)
+	print("Sucessfully decoded: " + decoded)
 	audio.close()	
 
 
